aacae2cb058c2368ba3d621efb976d7ac1e21c4f
Josh Presley <joshua.m.presley@icloud.com>
2022-04-23 16:15:26 -0400

    initial test components

---
+++ src/Components/container.js
@@ -0,0 +1,21 @@
+import './container.css'
+import { Component } from 'react';
+
+//This component will hold information for a song to be practiced. perhaps subclass this?
+class Container extends Component{
+
+    constructor(props){
+        super(props);
+        this.name = props.name;
+        this.position = props.position;
+    }
+
+    render(){
+        return (
+            <div className="container">
+                <p>{this.name} is at postion {this.position}</p>
+            </div>
+        );
+    }
+}
+export default Container;
