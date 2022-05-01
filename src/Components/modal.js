a9b89446e7d77e9e4a5b238d14d74ee1d459d689
Josh Presley <joshua.m.presley@icloud.com>
2022-05-01 12:34:35 -0400

    add modal file

---
+++ src/Components/modal.js
@@ -0,0 +1,48 @@
+import React, {Component} from "react";
+
+export default class Modal extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+
+    handleChange(event){
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event){
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+        this.onClose(event);
+    }
+
+    render(){
+        if(!this.props.show){
+            return null;
+        }
+        else{
+            return (
+                <div>
+                    <form onSubmit={this.handleSubmit}>        
+                        <label>
+                            Name:
+                            <input type="text" value={this.state.value} onChange={this.handleChange} />
+                        </label>
+                        <input type="submit" value="Submit" />
+                    </form>
+                    <button onClick={e => { this.onClose(e); }}>Close</button>
+                </div>
+            );
+        }
+    }
+
+    onClose = e => {
+        this.props.name = this.state.name;
+        this.props.onClose && this.props.onClose(e);
+    }
+}
