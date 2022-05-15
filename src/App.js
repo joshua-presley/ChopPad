af2f5a0541b1f876c2eb6df4aa768d1ff10db442
Josh Presley <joshua.m.presley@icloud.com>
2022-05-15 17:12:17 -0400

    getItems function

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import Container, { Material, NewMaterialItem } from './Components/container'
 import React from 'react';
 import ReactDOM from 'react'
 import Grid from './Components/grid'
+import { ReactSession } from 'react-client-session';
+
+const sqlite = require('sqlite3').verbose();
 
 
 function App() {
-
+  ReactSession.set("userid", "9C097FD9-74E2-4832-AD25-8B3FC0B8F36D");
   const items = [new NewMaterialItem('test', 'AABA', 'add your notes here'), new NewMaterialItem('test1', 'ABCD', 'add your notes here'), new NewMaterialItem('test2', 'ABA', 'add your notes here')]
   return (
     <div className="App">
@@ -21,5 +24,24 @@ function App() {
   );
 }
 
+function GetItems(containerName){
+  try{
+    db = new sqlite.Database('test.db');
+    const items = [];
+
+    containerTypes = ['NEWMATERIAL', 'DEVELOPINGMATERIAL', 'PERFORMANCEMATERIAL', 'TECHNIQUE', 'MUSICIANSHIP'];
+    
+    if(containerName in containerTypes){
+      db.Serialize(() => {
+        db.each("select title, form, notes from NewMaterial where userid = '" + ReactSession.Get('userid') + "' AND type = '" + containerName + "'", (err, row) => {
+          items.push(new NewMaterialItem(row.title, row.form, row.notes));
+        })
+      });
+    }
+  }
+  catch{
+    throw("error getting container items")
+  }
+}
 
 export default App;
